8f468fbd15b62261278fbdf80341d24a21ee8bc3
Ghaithzoaiter <ghaithzezotiktok@gmail.com>
2025-08-24 22:26:41 +0300

    new fix

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 import { ENV } from "./env.js";
 
-export const  connectDB = async () => {
-    try{
-        await mongoose.connect(ENV.MONGO_URI);
-        console.log("Connected to the DB successfully✅");
-    }catch(error){
-        console.error("Error connecting to the database");
-        process.exit(1);
-    }
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(ENV.MONGO_URI);
+    console.log("Connected to DB SUCCESSFULLY ✅");
+  } catch (error) {
+    console.log("Error connecting to MONGODB");
+    process.exit(1);
+  }
 };
